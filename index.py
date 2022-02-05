ed9b9931408628411b90c38988d498ace64d8c3b
chahdi <alaeddine.chahdi@edu.esiee.fr>
2022-02-05 14:25:28 +0100

    FEAT-200: the user gives their name and the function greets them with their name.

--- index.py
+++ index.py
@@ -1,5 +1,11 @@
 def hello():
     print('hello world')
 
+def name():
+    name=str(input("enter the name : "))
+    print('hello ' + str(name))
+    
+
 if __name__ == '__main__':
     hello()
+    name()
