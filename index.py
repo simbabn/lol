06bf6df7404a1028486bca7a1c4f03f22ce1b4bd
chahdi <alaeddine.chahdi@edu.esiee.fr>
2022-02-05 14:42:24 +0100

    FEAT-300: function that for a number “n” given by the user, it takes a for-loop and calculates the sum of the numbers from 1 to n.

--- index.py
+++ index.py
@@ -4,8 +4,17 @@ def hello():
 def name():
     name=str(input("enter the name : "))
     print('hello ' + str(name))
-    
+
+def sum():
+    #instanciate the two numbers
+    nbr1 = input('Entrez le premier nombre: ')
+    nbr2 = input('Entrez le deuxième nombre: ') 
+     
+    s = int(nbr1) + int(nbr2)
+    #printing
+    print(s)
 
 if __name__ == '__main__':
     hello()
     name()
+    sum()
